8337374b27706b1728a3bb68bf69ba6225249d47
enzoescipy <enzoescipy@gmail.com>
2024-01-10 17:09:38 +0900

    leave only docker-compose.yml, and env file. deleted all the other files completely.

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AppService {
-  getHello(): string {
-    return 'Welcome! this is nest.js + RESTful api test server!';
-  }
-}
